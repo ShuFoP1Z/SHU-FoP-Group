a6cd00e94255b3d8d9efda92f816189a7404c3da
ash12swanson <ash12swanson@googlemail.com>
2015-03-25 17:12:48 +0000

    Renamed the .cpp to 4374_Task1c_Skeleton.cpp
    Instead of with the 1A Complete.cpp

--- 4374_Task1c_Skeleton/4374_Task1c_Skeleton.cpp
+++ 4374_Task1c_Skeleton/4374_Task1c_Skeleton.cpp
@@ -2,39 +2,42 @@
 //---------------------------------
 //include libraries
 //include standard libraries
-#include <iostream >         //for output and input: cin >> and cout <<
-#include <iomanip>           //for formatted output in 'cout'
-#include <conio.h>           //for getch()
-#include <string>            //for string
+#include <iostream >						//for output and input: cin >> and cout <<
+#include <iomanip>							//for formatted output in 'cout'
+#include <conio.h>							//for getch()
+#include <string>							//for string
+#include <vector>							//for vectors
 using namespace std;
 
 //include our own libraries
-#include "RandomUtils.h"     //for Seed, Random
-#include "ConsoleUtils.h"    //for Clrscr, Gotoxy, etc.
+#include "RandomUtils.h"					//for Seed, Random
+#include "ConsoleUtils.h"					//for Clrscr, Gotoxy, etc.
 
 //---------------------------------
 //define constants
 //---------------------------------
 //define global constants
 //defining the size of the grid
-const int SIZEY(12);         //vertical dimension
-const int SIZEX(20);         //horizontal dimension
+const int SIZEY(12);						//vertical dimension
+const int SIZEX(20);						//horizontal dimension
 //defining symbols used for display of the grid and content
-const char SPOT('@');        //spot
-const char TUNNEL(' ');      //open space
-const char WALL('#');        //border
-//defining the command letters to move the blob on the maze
-const int  UP(72);           //up arrow
-const int  DOWN(80);         //down arrow
-const int  RIGHT(77);        //right arrow
-const int  LEFT(75);         //left arrow
+const char SPOT('@');						//spot
+const char TUNNEL(' ');						//open space
+const char WALL('#');						//border
+const char HOLE('O');						//hole
+//defining the command letters to move spot on the maze
+const int  UP(72);							//up arrow
+const int  DOWN(80);						//down arrow
+const int  RIGHT(77);						//right arrow
+const int  LEFT(75);						//left arrow
 //defining the other command letters
-const char QUIT('Q');        //end the game
+const char QUIT('Q');						//end the game
 
 //data structure to store data for a grid item
-struct Item {
-	const char symbol;	     //symbol on grid
-	int x, y;			     //coordinates
+struct Item
+{
+	const char symbol;						//symbol on grid
+	int x, y;								//coordinates
 };
 
 //---------------------------------------------------------------------------
@@ -44,80 +47,110 @@ struct Item {
 int main()
 {
 	//function declarations (prototypes)
-	void initialiseGame(char grid[][SIZEX], Item& spot);
+	void initialiseGame(char grid[][SIZEX], Item& spot, vector<Item>& holes);
 	bool wantToQuit(int k);
 	bool isArrowKey(int k);
+	bool outOfLives(int lives);
 	int  getKeyPress();
-	void updateGame(char g[][SIZEX], Item& sp, int k, string& mess);
-	void renderGame(const char g[][SIZEX], string mess);
-	void endProgram();
+	void updateGame(char g[][SIZEX], Item& sp, vector<Item> holes, int k, int& lives, string& mess);
+	void renderGame(const char g[][SIZEX], string mess, int lives);
+	void endProgram(int lives);
 
 	//local variable declarations 
-	char grid[SIZEY][SIZEX];                //grid for display
-	Item spot = { SPOT };                   //Spot's symbol and position (0, 0) 
-	string message("LET'S START...      "); //current message to player
+	char grid[SIZEY][SIZEX];								//grid for display
+	int lives(3);											//The number of lives spot has
+	Item spot = { SPOT };									//Spot's symbol and position (0, 0) 
+	Item hole = { HOLE };									//Hole's symbol and position (0, 0)
+	vector <Item> holes(12, hole);							//Creates a vector of holes, with each element being initialised as hole 
+	string message("LET'S START...      ");					//current message to player
+
+	bool running = true;
 
 	//action...
-	initialiseGame(grid, spot);           //initialise grid (incl. walls and spot)
-	int key(' ');                         //create key to store keyboard events
+	initialiseGame(grid, spot, holes);						//initialise grid (incl. walls and spot)
+	int key(' ');											//create key to store keyboard events
 	do {
-		renderGame(grid, message);        //render game state on screen
-		message = "                    "; //reset message
-		key = getKeyPress();              //read in next keyboard event
+		renderGame(grid, message, lives);					//render game state on screen
+		message = "                    ";					//reset message
+		key = getKeyPress();								//read in next keyboard event
 		if (isArrowKey(key))
-			updateGame(grid, spot, key, message);
+			updateGame(grid, spot, holes, key, lives, message);
 		else
-			message = "INVALID KEY!        "; //set 'Invalid key' message
-	} while (!wantToQuit(key));               //while user does not want to quit
-	endProgram();                             //display final message
+			message = "INVALID KEY!        ";				//set 'Invalid key' message
+
+		if (wantToQuit(key))								//if player wants to quit
+			running = false;
+		if (outOfLives(lives))								//if player is out of lives
+			running = false;
+	} while (running);
+
+	endProgram(lives);										//display final message
 	return 0;
 } //end main
 
-void updateGame(char grid[][SIZEX], Item& spot, int key, string& message)
+void updateGame(char grid[][SIZEX], Item& spot, vector<Item> holes, int key, int& lives, string& message)
 { //updateGame state
-	void updateSpotCoordinates(const char g[][SIZEX], Item& spot, int key, string& mess);
-	void updateGrid(char g[][SIZEX], Item spot);
+	void updateSpotCoordinates(const char g[][SIZEX], Item& spot, int key, int& lives, string& mess);
+	void updateGrid(char g[][SIZEX], Item spot, vector<Item> holes);
 
-	updateSpotCoordinates(grid, spot, key, message);    //update spot coordinates
-                                                        //according to key
-	updateGrid(grid, spot);                             //update grid information
+	updateSpotCoordinates(grid, spot, key, lives, message);	//update according to key
+	updateGrid(grid, spot, holes);							//update grid information
 }
 
 //---------------------------------------------------------------------------
 //----- initialise game state
 //---------------------------------------------------------------------------
 
-void initialiseGame(char grid[][SIZEX], Item& spot)
+void initialiseGame(char grid[][SIZEX], Item& spot, vector<Item>& holes)
 { //initialise grid and place spot in middle
 	void setGrid(char[][SIZEX]);
 	void setSpotInitialCoordinates(Item& spot);
+	void setHoleInitialCoordinates(vector<Item>& holes, Item& spot);
 	void placeSpot(char gr[][SIZEX], Item spot);
+	void placeHoles(char gr[][SIZEX], vector<Item> holes);
 
-	Seed();                            //seed reandom number generator
-	setSpotInitialCoordinates(spot);   //initialise spot position
-	setGrid(grid);                     //reset empty grid
-	placeSpot(grid, spot);             //set spot in grid
+	Seed();													//seed random number generator
+	
+	//do while?
+	setGrid(grid);											//reset empty grid
+	setSpotInitialCoordinates(spot);						//initialise spot position
+	placeSpot(grid, spot);									//set spot in grid
+	setHoleInitialCoordinates(holes, spot);					//intiialise holes position
+	placeHoles(grid, holes);								//set holes in grid
+
 } //end of initialiseGame
 
 void setSpotInitialCoordinates(Item& spot)
 { //set spot coordinates inside the grid at random at beginning of game
-	spot.y = Random(SIZEY - 2);      //vertical coordinate in range [1..(SIZEY - 2)]
-	spot.x = Random(SIZEX - 2);    //horizontal coordinate in range [1..(SIZEX - 2)]
-} //end of setSpotInitialoordinates
+	spot.y = Random(SIZEY - 2);								//vertical coordinate in range [1..(SIZEY - 2)]
+	spot.x = Random(SIZEX - 2);								//horizontal coordinate in range [1..(SIZEX - 2)]
+} //end of setSpotInitialCoordinates
+
+void setHoleInitialCoordinates(vector<Item>& holes, Item& spot)
+{ //set hole coordinates inside the grid at random at beginning of game
+	for (int i = 0; i < 12; ++i)
+	{
+		holes[i].y = Random(SIZEY - 2);						//vertical coordinate in range [1..(SIZEY - 2)]
+		holes[i].x = Random(SIZEX - 2);						//horizontal coordinate in range [1..(SIZEX - 2)]
+
+		if (holes[i].y == spot.y && holes[i].x == spot.x)	//if a hole is in the same place as spot
+			--i;											//then decrement i so the hole is moved somewhere else
+	}
+} //end of setHoleInitialCoordinates
 
 void setGrid(char grid[][SIZEX])
 { //reset the empty grid configuration
-	for (int row(0); row < SIZEY; ++row) //for each column
+	for (int row(0); row < SIZEY; ++row)				//for each column
 	{
-		for (int col(0); col < SIZEX; ++col) //for each col
+		for (int col(0); col < SIZEX; ++col)			//for each col
 		{
-			if ((row == 0) || (row == SIZEY - 1))     //top and bottom walls
-				grid[row][col] = WALL;                //draw a wall symbol
+			if ((row == 0) || (row == SIZEY - 1))		//top and bottom walls
+				grid[row][col] = WALL;					//draw a wall symbol
 			else
-				if ((col == 0) || (col == SIZEX - 1)) //left and right walls
-					grid[row][col] = WALL;            //draw a wall symbol
+				if ((col == 0) || (col == SIZEX - 1))	//left and right walls
+					grid[row][col] = WALL;				//draw a wall symbol
 				else
-					grid[row][col] = TUNNEL;          //draw a space
+					grid[row][col] = TUNNEL;			//draw a space
 		} //end of row-loop
 	} //end of col-loop
 } //end of setGrid
@@ -127,41 +160,51 @@ void placeSpot(char gr[][SIZEX], Item spot)
 	gr[spot.y][spot.x] = spot.symbol;
 } //end of placeSpot
 
+void placeHoles(char gr[][SIZEX], vector<Item> holes)
+{ //place holes at their new positions in grid
+	for (int i = 0; i < 12; ++i)
+		gr[holes[i].y][holes[i].x] = holes[i].symbol;
+} //end of placeHoles
+
 //---------------------------------------------------------------------------
 //----- update grid state
 //---------------------------------------------------------------------------
 
-void updateGrid(char grid[][SIZEX], Item spot)
+void updateGrid(char grid[][SIZEX], Item spot, vector<Item> holes)
 { //update grid configuration after each move
 	void setGrid(char[][SIZEX]);
 	void placeSpot(char g[][SIZEX], Item spot);
+	void placeHoles(char g[][SIZEX], vector<Item> holes);
 
-	setGrid(grid);	         //reset empty grid
-	placeSpot(grid, spot);	 //set spot in grid
+	setGrid(grid);							//reset empty grid
+	placeHoles(grid, holes);				//set holes in grid
+	placeSpot(grid, spot);					//set spot in grid
 } //end of updateGrid
 
 //---------------------------------------------------------------------------
 //----- move the spot
 //---------------------------------------------------------------------------
-void updateSpotCoordinates(const char g[][SIZEX], Item& sp, int key, string& mess)
+void updateSpotCoordinates(const char g[][SIZEX], Item& sp, int key, int& lives, string& mess)
 { //move spot in required direction
 	void setKeyDirection(int k, int& dx, int& dy);
 
 	//calculate direction of movement required by key - if any
 	int dx(0), dy(0);
-	setKeyDirection(key, dx, dy); 	//find direction indicated by key
+	setKeyDirection(key, dx, dy);			//find direction indicated by key
 	//check new target position in grid 
 	//and update spot coordinates if move is possible
 	const int targetY(sp.y + dy);
 	const int targetX(sp.x + dx);
 	switch (g[targetY][targetX])
-	{		//...depending on what's on the target position in grid...
-	case TUNNEL:      //can move
-		sp.y += dy;   //go in that Y direction
-		sp.x += dx;   //go in that X direction
+	{//...depending on what's on the target position in grid...
+	case HOLE:								//can move
+		--lives;
+	case TUNNEL:
+		sp.y += dy;							//go in that Y direction
+		sp.x += dx;							//go in that X direction
 		break;
-	case WALL:        //hit a wall and stay there
-		cout << '\a'; //beep the alarm
+	case WALL:								//hit a wall and stay there
+		cout << '\a';						//beep the alarm
 		mess = "CANNOT GO THERE!    ";
 		break;
 	}
@@ -174,21 +217,21 @@ void setKeyDirection(int key, int& dx, int& dy)
 { //
 	switch (key)    //...depending on the selected key...
 	{
-	case LEFT:      //when LEFT arrow pressed...
-		dx = -1;    //decrease the X coordinate
+	case LEFT:			//when LEFT arrow pressed...
+		dx = -1;		//decrease the X coordinate
 		dy = 0;
 		break;
-	case RIGHT:     //when RIGHT arrow pressed...
-		dx = +1;    //increase the X coordinate
+	case RIGHT:			//when RIGHT arrow pressed...
+		dx = +1;		//increase the X coordinate
 		dy = 0;
 		break;
-	case UP:
-		dx = 0; 
-		dy = -1;
-		break; 
-	case DOWN:
-		dx = 0; 
-		dy = +1;
+	case UP:			//when UP arrow pressed...
+		dx = 0;
+		dy = -1;		//decrease the Y coordinate
+		break;
+	case DOWN:			//when DOWN arrow pressed...
+		dx = 0;
+		dy = +1;		//increase the Y coordinate
 		break;
 	}
 } //end of setKeyDirection
@@ -196,8 +239,8 @@ void setKeyDirection(int key, int& dx, int& dy)
 int getKeyPress()
 { //get key or command selected by user
 	int keyPressed;
-	keyPressed = getch();      //read in the selected arrow key or command letter
-	while (keyPressed == 224)     //ignore symbol following cursor key
+	keyPressed = getch();			//read in the selected arrow key or command letter
+	while (keyPressed == 224)		//ignore symbol following cursor key
 		keyPressed = getch();
 	return(keyPressed);   
 } //end of getKeyPress
@@ -212,6 +255,13 @@ bool wantToQuit(int key)
 	return (key == QUIT);
 } //end of wantToQuit
 
+bool outOfLives(int lives)
+{ //check if spot has lives
+	if (lives > 0)
+		return false;
+	else
+		return true;
+} //end of outOfLives
 
 //---------------------------------------------------------------------------
 //----- display info on screen
@@ -220,18 +270,18 @@ void clearMessage()
 { //clear message area on screen
 	SelectBackColour(clBlack);
 	SelectTextColour(clWhite);
-	Gotoxy(40, 8);
+	Gotoxy(40, 9);
 	string str(20, ' ');
-	cout << str;  //display blank message
+	cout << str;								//display blank message
 
 } //end of setMessage
 
-void renderGame(const char gd[][SIZEX], string mess)
+void renderGame(const char gd[][SIZEX], string mess, int lives)
 { //display game title, messages, maze, spot and apples on screen
 	void paintGrid(const char g[][SIZEX]);
 	void showTitle();
 	void showOptions();
-	void showMessage(string);
+	void showMessage(string, int lives);
 
 	Gotoxy(0, 0);
 	//display grid contents
@@ -241,7 +291,7 @@ void renderGame(const char gd[][SIZEX], string mess)
 	//display menu options available
 	showOptions();
 	//display message if any
-	showMessage(mess);
+	showMessage(mess, lives);
 } //end of paintGame
 
 void paintGrid(const char g[][SIZEX])
@@ -249,11 +299,11 @@ void paintGrid(const char g[][SIZEX])
 	SelectBackColour(clBlack);
 	SelectTextColour(clWhite);
 	Gotoxy(0, 2);
-	for (int row(0); row < SIZEY; ++row)      //for each row (vertically)
+	for (int row(0); row < SIZEY; ++row)		//for each row (vertically)
 	{
-		for (int col(0); col < SIZEX; ++col)  //for each column (horizontally)
+		for (int col(0); col < SIZEX; ++col)	//for each column (horizontally)
 		{
-			cout << g[row][col];              //output cell content
+			cout << g[row][col];				//output cell content
 		} //end of col-loop
 		cout << endl;
 	} //end of row-loop
@@ -267,34 +317,41 @@ void showTitle()
 	SelectBackColour(clWhite);
 	SelectTextColour(clRed);
 	Gotoxy(40, 0);
-	cout << "Pascale Vacher: March 15";
+	cout << "Ashley Swanson: March 15";
 } //end of showTitle
 
 
 void showOptions()
 { //show game options
+
 	SelectBackColour(clRed);
 	SelectTextColour(clYellow);
 	Gotoxy(40, 5);
 	cout << "TO MOVE USE KEYBOARD ARROWS  ";
 	Gotoxy(40, 6);
 	cout << "TO QUIT ENTER 'Q'   ";
+
 } //end of showOptions
 
-void showMessage(string m)
+void showMessage(string m, int lives)
 { //print auxiliary messages if any
 	SelectBackColour(clBlack);
 	SelectTextColour(clWhite);
-	Gotoxy(40, 8);
+	Gotoxy(40, 9);
 	cout << m;	//display current message
+	Gotoxy(40, 11);
+	cout << "You have " << lives << " lives left";
 } //end of showMessage
 
-void endProgram()
+void endProgram(int lives)
 { //end program with appropriate message
 	SelectBackColour(clBlack);
 	SelectTextColour(clYellow);
 	Gotoxy(40, 8);
-	cout << "PLAYER QUITS!          ";
+	if (outOfLives(lives))
+		cout << "YOU LOST!              ";
+	else
+		cout << "PLAYER QUITS!          ";
 	//hold output screen until a keyboard key is hit
 	Gotoxy(40, 9);
 	system("pause");
